355a700535a8d8d14e5049dc40995b0933060892
marandabui <73601350+marandabui@users.noreply.github.com>
2024-02-21 12:48:09 -0500

    added function for after a user selects a category and amount

--- Game System/trivia.js
+++ Game System/trivia.js
@@ -1,68 +1,96 @@
-//https://opentdb.com/api.php?amount=10
 
-//NOTE: jacob needs to add elements to connect question and answer options to front end elements
-
-let rightAnswer = "";
 let correctScore = 0;
 let questionsDOne = 0;
 let totalQuestions = 25;
-
+let category = "";
+let amount = 0;
+let musicQuestions = "";
 
 async function getCategoryMusic(){
     //music
     const categoryURLMusic = 'https://opentdb.com/api.php?amount=5&category=12&difficulty=easy&type=boolean';
-    const categoryMusic = await fetch(`${categoryURLMusic}`);
-    const dataCategoryMusic = await categoryMusic.json();
-    console.log(dataCategoryMusic);
+    const results = await fetch(`${categoryURLMusic}`);
+    const dataCategoryMusic = await results.json();
+    //console.log(dataCategoryMusic);
+
+    return dataCategoryMusic;   
     
 }
+
 
 async function getCategoryAnimals(){
      //animals
     
      const categoryURLAnimals = 'https://opentdb.com/api.php?amount=5&category=27&difficulty=easy&type=boolean';
-     const categoryAnimals = await fetch(`${categoryURLAnimals}`);
-     const dataCategoryAnimals = await categoryAnimals.json();
-     console.log(dataCategoryAnimals);
+     const results = await fetch(`${categoryURLAnimals}`);
+     const dataCategoryAnimals = await results.json();
+     //console.log(dataCategoryAnimals);
+     return dataCategoryAnimals;
 }
 
 async function getCategorySports(){
     //sports
-    //https://opentdb.com/api.php?amount=5&category=21
     const categoryURLSports = 'https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=boolean';
-    const categorySports = await fetch(`${categoryURLSports}`);
-    const dataCategorySports = await categorySports.json();
-    console.log(dataCategorySports);
+    const results = await fetch(`${categoryURLSports}`);
+    const dataCategorySports = await results.json();
+    //console.log(dataCategorySports);
+    return dataCategorySports;
 
 }
 
 async function getCategoryHistory(){
     //history
-    //https://opentdb.com/api.php?amount=5&category=23 
     const categoryURLHistory = 'https://opentdb.com/api.php?amount=5&category=23&difficulty=easy&type=boolean';
-    const categoryHistory = await fetch(`${categoryURLHistory}`);
-    const dataCategoryHistory = await categoryHistory.json();
-    console.log(dataCategoryHistory);
+    const results = await fetch(`${categoryURLHistory}`);
+    const dataCategoryHistory = await results.json();
+    //console.log(dataCategoryHistory);
+    return dataCategoryHistory;
 
 }
 
 async function getCategoryMovies(){
     //movies
-    //https://opentdb.com/api.php?amount=5&category=11
     const categoryURLMovies = 'https://opentdb.com/api.php?amount=5&category=11&difficulty=easy&type=boolean';
-    const categoryMovies = await fetch(`${categoryURLMovies}`);
-    const dataCategoryMovies = await categoryMovies.json();
-    console.log(dataCategoryMovies);
+    const results = await fetch(`${categoryURLMovies}`);
+    const dataCategoryMovies = await results.json();
+    //console.log(dataCategoryMovies);
+    return dataCategoryMovies;
 
 }
 
 
 
-getCategoryMusic();
-setTimeout(getCategoryAnimals, 5000);
-setTimeout(getCategorySports, 10000);
-setTimeout(getCategoryHistory, 15000);
-setTimeout(getCategoryMovies, 20000);
+//have a function once user selects category and difficulty then display question
+//TODO: instead of having console.log(), display the question to the front-end 
+function question(category, amount){
+    switch(category){
+        case "Entertainment: Music":
+            getCategoryMusic().then((value)=>{console.log(value.results[0].question)});
+            break;
+        case "Animals":
+            getCategoryAnimals().then((value)=>{console.log(value.results[0].question)});
+            break;
+        case "Sports":
+            getCategorySports().then((value)=>{console.log(value.results[0].question)});
+            break;
+        case "History":
+            getCategoryHistory().then((value)=>{console.log(value.results[0].question)});
+            break;
+        case "Movies":
+            getCategoryMovies().then((value)=>{console.log(value.results[0].question)});  
+            break;    
+
+    }
+}
+
+//console.log(question("Entertainment: Music", 100));
+//console.log(question("Animals", 200));
+//console.log(question("Sports", 300));
+//console.log(question("History", 400));
+//console.log(question("Movies", 500));
+
+
+    
 
 
 
