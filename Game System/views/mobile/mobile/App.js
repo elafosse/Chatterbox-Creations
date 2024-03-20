31ddcb7ecb39f82f609c87163392265c9b61b9f3
Gabrielle Richards <gabbynicole@comcast.net>
2024-03-20 17:08:58 -0400

    setup

---
+++ Game System/views/mobile/mobile/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ Game System/views/mobile/mobile/screens/HomeScreen.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 24px;
+`;
+
+const Input = styled.TextInput`
+  height: 50px;
+  margin-vertical: 10px;
+  padding-horizontal: 15px;
+  border-radius: 25px;
+  border: 1px solid #ccc;
+  width: 80%;
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: #0000ff;
+  border-radius: 25px;
+  width: 80%;
+  padding: 15px;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const ButtonText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Container>
+      <Title>Room Code</Title>
+      <Input placeholder="XXXX" />
+      <Title>Name</Title>
+      <Input placeholder="Johnny" />
+      <Button onPress={() => {/* Handle the press event */}}>
+        <ButtonText>PLAY</ButtonText>
+      </Button>
+    </Container>
+  );
+};
+
+export default HomeScreen;
