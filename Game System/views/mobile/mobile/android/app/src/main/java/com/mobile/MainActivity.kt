d49b2fcfe43c3dc68c7174c7cb20da0dc78facb1
Gabrielle Richards <gabbynicole@comcast.net>
2024-03-21 01:15:36 -0400

    more set up

--- Game System/views/mobile/mobile/android/app/src/main/java/com/mobile/MainActivity.kt
+++ Game System/views/mobile/mobile/android/app/src/main/java/com/mobile/MainActivity.kt
@@ -2,6 +2,8 @@ package com.mobile
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
