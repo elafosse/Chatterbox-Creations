511a4b11aa715524fef176d98203cf6bab8e39b7
Endrick Lafosse <elafosse@outlook.com>
2024-04-24 13:00:45 -0400

    New MemoryStore Added

--- Game System/index.js
+++ Game System/index.js
@@ -1,23 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const MemoryStore = require('memorystore')(session)
 const routes = require('./modules/routes');
-const redis = require('redis');
-const RedisStore = require('connect-redis')(session);
 
 const app = express();
 const port = process.env.PORT || 8080;
 const secret = process.env.SECRET_KEY || 'default-secret';
-const redisClient = redis.createClient();
 process.env.PWD = process.cwd()
 
 const middleware = session({
   secret: secret,
   resave: true,
   saveUninitialized: true,
-  store: new RedisStore({ client: redisClient }),
+  store: new MemoryStore({ checkPeriod: 86400000 }),
 })
-
 
 // Front End
 app.disable("x-powered-by");
